754323113fd66349c7954cb1c5970d689df25ab5
dlwjdals102 <35514847+dlwjdals102@users.noreply.github.com>
2026-03-16 22:16:04 +0900

    skybox asset import / obstacle spawner
    필요한 에셋 추가
    장애물 + 장애물 생성기 추가
    장애물 기본 베이스 프리펩을 만들고, 다양한 장애물들을 prefab variant로 늘려갈 예정.

---
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject obstaclePrefab;
+    [SerializeField] float obstacleSpawnTime = 1f;
+
+    void Start()
+    {
+        StartCoroutine(SpawnObstacleRoutine());
+    }
+
+    IEnumerator SpawnObstacleRoutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(obstacleSpawnTime);
+            Instantiate(obstaclePrefab, transform.position, Random.rotation);
+        }
+    }
+}
